e4efad9cb145b24f9caf874a03d45b5f6347058d
Petro <peter.k.nets@gmail.com>
2024-03-02 13:25:08 +0200

    Set up Hero section

---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<script></script>
+
+<header class="flex flex-col relative z-20">
+  <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between px-4 py-6">
+    <a href="/">
+      <h1 class="font-semibold">Swoley <span class="text-indigo-400">Moley</span></h1>
+    </a>
+    <button class="md:hidden grid place-items-center"><i class="fa-solid fa-bars"></i></button>
+    <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+      <a href="#product" class="cursor-pointer hover:text-indigo-400 duration-200">Product</a>
+      <a href="#reviews" class="cursor-pointer hover:text-indigo-400 duration-200">Reviews</a>
+      <a href="#faqs" class="cursor-pointer hover:text-indigo-400 duration-200">FAQs</a>
+      <button class="specialBtn">
+        <p>Start free today</p>
+      </button>
+    </nav>
+  </div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
   import "../app.css";
+  import Footer from "../components/Footer.svelte";
 </script>
 
 <slot />
+<Footer />
+<!--
+hero
+product description
+user reviews
+faw
+conversion
+footer
+-->
