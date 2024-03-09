345371cdb9c74f87b8a4374b771e4189b80bf580
Petro <peter.k.nets@gmail.com>
2024-03-09 13:16:26 +0200

    Fill in the remaining code

--- src/components/CTAs.svelte
+++ src/components/CTAs.svelte
@@ -1,10 +1,12 @@
-<script></script>
+<script>
+    export let dark;
+</script>
 
 <div class="flex items-center gap-4">
-  <button class="specialBtn">
-    <p class="text-base sm:text-lg md:text-xl">Contact Us</p>
-  </button>
-  <button class="specialBtnDark">
-    <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p>
-  </button>
+    <button class={"specialBtn " + (dark ? " bg-[#181b34]" : " ")}>
+        <p class="text-base sm:text-lg md:text-xl">Contact Us</p>
+    </button>
+    <button class="specialBtnDark">
+        <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p>
+    </button>
 </div>
